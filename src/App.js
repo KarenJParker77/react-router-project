6991281912e507f8cf761f4acb681e3c20199979
KarenJParker77 <karen.mccrackle@gmail.com>
2023-07-10 20:29:15 +0100

    added NavLinks and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 function App() {
   return (
-    <div className="App">
-      Hello, Ninjas!
-    </div>
+    <BrowserRouter>
+      <header>
+        <nav>
+          <h1>Jobseeker</h1>
+          {/* NavLink can give us a class of active whenever we're on that page. Link can't do this */}
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
